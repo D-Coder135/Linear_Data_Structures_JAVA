a9791928eff0965c8d42a0c9aeffc1de1e004848
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-05-30 15:04:41 +0530

    add a class name KWArrayList.java inside the package arrayList

---
+++ src/arrayList/KWArrayList.java
@@ -0,0 +1,11 @@
+/*   Created by IntelliJ IDEA.
+ *   Author: Devansh Shukla (D_Coder135)
+ *   Date: 30/05/21
+ *   Time: 3:04 PM
+ *   File: KWArrayList.java
+ */
+
+package arrayList;
+
+public class KWArrayList {
+}
