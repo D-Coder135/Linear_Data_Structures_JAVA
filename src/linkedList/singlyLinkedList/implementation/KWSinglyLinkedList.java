ce6ff1631199e5612afca7b5d8e029fdbe4b212e
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-05-31 23:09:03 +0530

    add some initialization statement

--- src/linkedList/singlyLinkedList/implementation/KWSinglyLinkedList.java
+++ src/linkedList/singlyLinkedList/implementation/KWSinglyLinkedList.java
@@ -99,5 +99,7 @@ public class KWSinglyLinkedList<E> {
             throw new IndexOutOfBoundsException(Integer.toString(index));
         }
         Node<E> node = getNode(index);
+        E result = node.getData();
+        node.setData(newValue);
     }
 }
