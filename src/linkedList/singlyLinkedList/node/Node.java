3654825a1d462d56c3e0e34220fad8566206dae9
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-05-31 21:56:18 +0530

    add a class Node.java inside the package singlyLinkedList/node

---
+++ src/linkedList/singlyLinkedList/node/Node.java
@@ -0,0 +1,11 @@
+/*   Created by IntelliJ IDEA.
+ *   Author: Devansh Shukla (D_Coder135)
+ *   Date: 31/05/21
+ *   Time: 9:55 PM
+ *   File: Node.java
+ */
+
+package linkedList.singlyLinkedList.node;
+
+public class Node {
+}
