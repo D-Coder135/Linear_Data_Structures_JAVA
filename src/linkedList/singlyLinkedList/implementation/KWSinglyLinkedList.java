8247bad4544c9068c084fab55d99c25623330537
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-05-31 22:13:19 +0530

    add a class name KWSinglyLinkedList.java inside the package singlyLinkedList/implementation

---
+++ src/linkedList/singlyLinkedList/implementation/KWSinglyLinkedList.java
@@ -0,0 +1,11 @@
+/*   Created by IntelliJ IDEA.
+ *   Author: Devansh Shukla (D_Coder135)
+ *   Date: 31/05/21
+ *   Time: 10:12 PM
+ *   File: KWSinglyLinkedList.java
+ */
+
+package linkedList.singlyLinkedList.implementation;
+
+public class KWSinglyLinkedList {
+}
