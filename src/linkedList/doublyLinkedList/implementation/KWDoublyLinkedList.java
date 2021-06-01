6bcc088b6594c0e8bbed33c4b86813368afcb39a
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-06-01 13:45:42 +0530

    add some comment to the code in the form of docs

--- src/linkedList/doublyLinkedList/implementation/KWDoublyLinkedList.java
+++ src/linkedList/doublyLinkedList/implementation/KWDoublyLinkedList.java
@@ -8,4 +8,8 @@
 package linkedList.doublyLinkedList.implementation;
 
 public class KWDoublyLinkedList<E> {
+    /*
+     * This field will store the reference to the first node in the linked list. i.e head
+     * */
+
 }
