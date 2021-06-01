caf2cf0c01da897963e20a3c766f8d8bfe2f7a14
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-06-01 13:28:17 +0530

    add the getter and setter methods for the private fields

--- src/linkedList/doublyLinkedList/node/Node.java
+++ src/linkedList/doublyLinkedList/node/Node.java
@@ -37,4 +37,28 @@ public class Node<E> {
         this.next = next;
         this.prev = prev;
     }
+
+    public E getData() {
+        return data;
+    }
+
+    public void setData(E data) {
+        this.data = data;
+    }
+
+    public Node<E> getNext() {
+        return next;
+    }
+
+    public void setNext(Node<E> next) {
+        this.next = next;
+    }
+
+    public Node<E> getPrev() {
+        return prev;
+    }
+
+    public void setPrev(Node<E> prev) {
+        this.prev = prev;
+    }
 }
