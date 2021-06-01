008c5d68b905ffe0c40549616b7202fe77d658b5
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-06-01 13:15:45 +0530

    add a class Node.java inside the package doublyLinkedList/node

---
+++ src/linkedList/doublyLinkedList/node/Node.java
@@ -0,0 +1,11 @@
+/*   Created by IntelliJ IDEA.
+ *   Author: Devansh Shukla (D_Coder135)
+ *   Date: 01/06/21
+ *   Time: 1:15 PM
+ *   File: Node.java
+ */
+
+package linkedList.doublyLinkedList.node;
+
+public class Node {
+}
