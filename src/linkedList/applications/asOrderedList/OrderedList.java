63a9b8433c9340e849258dbaa491940d3089e25a
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-06-02 20:12:05 +0530

    call the add() method along with the iter object and pass the obj field to it as the parameter

--- src/linkedList/applications/asOrderedList/OrderedList.java
+++ src/linkedList/applications/asOrderedList/OrderedList.java
@@ -41,7 +41,7 @@ public class OrderedList<E extends Comparable<E>> implements Iterable<E> {
         }
         // assert: All items were examined and no item is larger than the element to be inserted.
         // Add the new item to the end of the list.
-
+        iter.add(obj);
     }
 
     @Override
