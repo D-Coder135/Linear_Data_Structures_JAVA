825b7594edbed24c16c665e5ab64b0c8cb23344d
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-06-02 19:44:35 +0530

    add a class OrderedList.java inside the package linkedList/applications/asOrderedList

---
+++ src/linkedList/applications/asOrderedList/OrderedList.java
@@ -0,0 +1,11 @@
+/*   Created by IntelliJ IDEA.
+ *   Author: Devansh Shukla (D_Coder135)
+ *   Date: 02/06/21
+ *   Time: 7:44 PM
+ *   File: OrderedList.java
+ */
+
+package linkedList.applications.asOrderedList;
+
+public class OrderedList {
+}
