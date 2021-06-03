391f651d1b7a185016154151c151ed2ce017ce2e
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-06-03 18:54:51 +0530

    add a class TestOrderedList.java inside the package asOrderedList/execution

---
+++ src/linkedList/applications/asOrderedList/execution/TestOrderedList.java
@@ -0,0 +1,11 @@
+/*   Created by IntelliJ IDEA.
+ *   Author: Devansh Shukla (D_Coder135)
+ *   Date: 03/06/21
+ *   Time: 6:54 PM
+ *   File: TestOrderedList.java
+ */
+
+package linkedList.applications.asOrderedList.execution;
+
+public class TestOrderedList {
+}
