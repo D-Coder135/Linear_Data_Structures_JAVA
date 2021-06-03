d000387bc9ed1c9981a335a5fa150886748bec52
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-06-03 18:59:47 +0530

    add a public static method of type void name traverseAndShow() along with a parameter inside it of type OrderedList<Integer>

--- src/linkedList/applications/asOrderedList/execution/TestOrderedList.java
+++ src/linkedList/applications/asOrderedList/execution/TestOrderedList.java
@@ -7,10 +7,16 @@
 
 package linkedList.applications.asOrderedList.execution;
 
+import linkedList.applications.asOrderedList.implementation.OrderedList;
+
 public class TestOrderedList {
     /**
      * Traverses ordered list and displays each element.
      * Displays an wrror meassage if an element is out of order.
+     *
      * @param testList An ordered list of integers
-     * */
+     */
+    public static void traverseAndShow(OrderedList<Integer> testList) {
+
+    }
 }
