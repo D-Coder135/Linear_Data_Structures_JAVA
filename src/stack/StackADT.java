5a2d4736a8bee8cea0519783d797e1e809f5cead
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-06-04 20:14:27 +0530

    add some comments in the form of docs

--- src/stack/StackADT.java
+++ src/stack/StackADT.java
@@ -7,5 +7,12 @@
 
 package stack;
 
+/**
+ * A Stack is a data structure in which objects are inserted into and remove fromt
+ * the same end (i.e., Last-In, First-Out).
+ *
+ * @param <E> The element type
+ */
+
 public interface StackADT {
 }
