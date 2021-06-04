6c23ad849afb488c9ca29701c69667fe90d04823
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-06-04 20:12:48 +0530

    add a interface StackADT inside the package stack

---
+++ src/stack/StackADT.java
@@ -0,0 +1,11 @@
+/*   Created by IntelliJ IDEA.
+ *   Author: Devansh Shukla (D_Coder135)
+ *   Date: 04/06/21
+ *   Time: 8:12 PM
+ *   File: StackADT.java
+ */
+
+package stack;
+
+public interface StackADT {
+}
