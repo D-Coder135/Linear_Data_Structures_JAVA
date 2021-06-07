fd8b78c26777d1787ccb3e64d147e0ba45e36850
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-06-07 21:13:07 +0530

    add the getter and setter methods for the private fields inside the inner class Node<E>

--- src/stack/usingLinkedList/implementation/LinkedListStack.java
+++ src/stack/usingLinkedList/implementation/LinkedListStack.java
@@ -32,6 +32,22 @@ private static class Node<E> {
         this.data = data;
         this.next = next;
     }
+
+    public E getData() {
+        return data;
+    }
+
+    public void setData(E data) {
+        this.data = data;
+    }
+
+    public Node<E> getNext() {
+        return next;
+    }
+
+    public void setNext(Node<E> next) {
+        this.next = next;
+    }
 }
 
     @Override
