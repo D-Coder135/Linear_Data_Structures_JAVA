5c6046e39c70f5226311e87ac10fc5fb2f0c99df
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-06-07 21:10:58 +0530

    add a parametrized constructor inside the class Node<E>

--- src/stack/usingLinkedList/implementation/LinkedListStack.java
+++ src/stack/usingLinkedList/implementation/LinkedListStack.java
@@ -22,6 +22,11 @@ private static class Node<E> {
 
     /* The reference to the next node. */
     private Node<E> next;
+
+    public Node(E data, Node<E> next) {
+        this.data = data;
+        this.next = next;
+    }
 }
 
     @Override
