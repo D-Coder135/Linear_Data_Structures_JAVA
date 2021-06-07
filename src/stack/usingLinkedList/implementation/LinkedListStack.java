727afb94616f0db017fb2ae6d3d8236bdc886102
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-06-07 21:04:40 +0530

    add a private inner class Node inside the class LinkedListStack.java

--- src/stack/usingLinkedList/implementation/LinkedListStack.java
+++ src/stack/usingLinkedList/implementation/LinkedListStack.java
@@ -15,7 +15,9 @@ import stack.StackADT;
 public class LinkedListStack<E> implements StackADT<E> {
 
 //    Inner class Node<E>.
+private class Node {
 
+}
 
     @Override
     public E push(E obj) {
