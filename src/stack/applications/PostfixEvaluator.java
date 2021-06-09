a541455e8d978dfd7c7c83b1a93d7be33f8fc9d4
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-06-09 18:12:49 +0530

    initialize the field result as per the case.

--- src/stack/applications/PostfixEvaluator.java
+++ src/stack/applications/PostfixEvaluator.java
@@ -47,6 +47,9 @@ public class PostfixEvaluator {
         // Evaluate the operator.
         switch (op) {
             case '+':
+                result = lhs + rhs;
+                break;
+
         }
     }
 }
