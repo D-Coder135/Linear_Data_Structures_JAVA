1f28c47e3342d1016aa0ecf6bf0968fbc580f651
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-06-09 17:37:26 +0530

    add a class PostfixEvaluator.java inside the package stack/applications

---
+++ src/stack/applications/PostfixEvaluator.java
@@ -0,0 +1,11 @@
+/*   Created by IntelliJ IDEA.
+ *   Author: Devansh Shukla (D_Coder135)
+ *   Date: 09/06/21
+ *   Time: 5:37 PM
+ *   File: PostfixEvaluator.java
+ */
+
+package stack.applications;
+
+public class PostfixEvaluator {
+}
