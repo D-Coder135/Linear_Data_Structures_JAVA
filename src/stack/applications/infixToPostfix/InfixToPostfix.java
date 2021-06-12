9e499afa1a9fbec288166785fc80036c21bb2dbb
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-06-12 22:22:24 +0530

    add a new class InfixToPostfix.java inside the package infixToPostfix inside the applications package

---
+++ src/stack/applications/infixToPostfix/InfixToPostfix.java
@@ -0,0 +1,11 @@
+/*   Created by IntelliJ IDEA.
+ *   Author: Devansh Shukla (D_Coder135)
+ *   Date: 12/06/21
+ *   Time: 10:21 PM
+ *   File: InfixToPostfix.java
+ */
+
+package stack.applications.infixToPostfix;
+
+public class InfixToPostfix {
+}
