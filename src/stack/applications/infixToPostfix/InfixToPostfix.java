59504b68cebc08a552684fb6c5ab551627f80e5c
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-06-14 23:46:23 +0530

    add a if() block along with a condition inside it

--- src/stack/applications/infixToPostfix/InfixToPostfix.java
+++ src/stack/applications/infixToPostfix/InfixToPostfix.java
@@ -140,6 +140,9 @@ public class InfixToPostfix {
                 // Pop all stacked operators with equal or high precedence than op.
                 while (!OPERATOR_STACK.isEmpty() && precedence(op) <= precedence(topOp)) {
                     OPERATOR_STACK.pop();
+                    if (topOp == '(') {
+
+                    }
                     POSTFIX.add(Character.toString(topOp));
                     if (!OPERATOR_STACK.isEmpty()) {
                         // Reset topOp
