122c2ee38c437659853cc963fb4236b15fca94bd
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-06-26 23:02:32 +0530

    add the getter and setter methods for the private field inside the class Node.java

--- src/linkedList/circular/singly/implementation/Node.java
+++ src/linkedList/circular/singly/implementation/Node.java
@@ -15,4 +15,20 @@ public class Node<E> {
         this.data = data;
         this.next = null;
     }
+
+    public E getData() {
+        return data;
+    }
+
+    public void setData(E data) {
+        this.data = data;
+    }
+
+    public Node<E> getNext() {
+        return next;
+    }
+
+    public void setNext(Node<E> next) {
+        this.next = next;
+    }
 }
