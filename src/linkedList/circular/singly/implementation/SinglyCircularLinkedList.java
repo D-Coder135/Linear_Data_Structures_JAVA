45de1a96500f33f5484b11023e9e52e9af22fe9e
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-06-26 23:05:23 +0530

    implement all the methods of the interface SinglyCircularLinkedListADT.java

--- src/linkedList/circular/singly/implementation/SinglyCircularLinkedList.java
+++ src/linkedList/circular/singly/implementation/SinglyCircularLinkedList.java
@@ -10,4 +10,28 @@ package linkedList.circular.singly.implementation;
 import linkedList.circular.singly.myInterfaceVersion.SinglyCircularLinkedListADT;
 
 public class SinglyCircularLinkedList implements SinglyCircularLinkedListADT {
+    @Override
+    public void addFirst(int data) {
+
+    }
+
+    @Override
+    public void addLast(int data) {
+
+    }
+
+    @Override
+    public int removeFirst() {
+        return 0;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return false;
+    }
+
+    @Override
+    public int size() {
+        return 0;
+    }
 }
