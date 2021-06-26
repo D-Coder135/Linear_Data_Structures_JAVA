a42ae5cf5c9e9dfaf0684a59855987f50c848adf
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-06-26 23:01:12 +0530

    add a parametrized constructor with a parameter inside it

--- src/linkedList/circular/singly/implementation/Node.java
+++ src/linkedList/circular/singly/implementation/Node.java
@@ -10,4 +10,8 @@ package linkedList.circular.singly.implementation;
 public class Node<E> {
     private E data;
     private Node<E> next;
+
+    public Node(E data) {
+        this.data = data;
+    }
 }
