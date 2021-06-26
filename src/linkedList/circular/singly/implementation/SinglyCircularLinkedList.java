3367bbeb2f9893382e226684d44510ea4901bfe4
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-06-26 23:04:03 +0530

    add a new class SinglyCircularLinkedList.java inside the package circular/singly/implementation

---
+++ src/linkedList/circular/singly/implementation/SinglyCircularLinkedList.java
@@ -0,0 +1,11 @@
+/*   Created by IntelliJ IDEA.
+ *   Author: Devansh Shukla (D_Coder135)
+ *   Date: 26/06/21
+ *   Time: 11:03 PM
+ *   File: SinglyCircularLinkedList.java
+ */
+
+package linkedList.circular.singly.implementation;
+
+public class SinglyCircularLinkedList {
+}
