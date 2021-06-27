8b7821816288e3a6c1b0991c78fdb1f550224b56
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-06-27 23:32:27 +0530

    call the addFirst() method

--- src/linkedList/circular/singly/execution/Main.java
+++ src/linkedList/circular/singly/execution/Main.java
@@ -13,6 +13,6 @@ public class Main {
     public static void main(String[] args) {
         SinglyCircularLinkedList<Integer> circularLinkedList = new SinglyCircularLinkedList<>();
         System.out.println(circularLinkedList.size());
-
+        circularLinkedList.addFirst(20);
     }
 }
