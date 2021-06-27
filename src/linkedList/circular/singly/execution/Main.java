5461f06cb93b039cd773c32210ba5d53ea78a2ec
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-06-27 23:27:37 +0530

    add a class Main.java inside the new package execution

---
+++ src/linkedList/circular/singly/execution/Main.java
@@ -0,0 +1,11 @@
+/*   Created by IntelliJ IDEA.
+ *   Author: Devansh Shukla (D_Coder135)
+ *   Date: 27/06/21
+ *   Time: 11:27 PM
+ *   File: Main.java
+ */
+
+package linkedList.circular.singly.execution;
+
+public class Main {
+}
