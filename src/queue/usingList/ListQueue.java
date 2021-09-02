8c90a298f01561948d1f045ed5ddf82420044057
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-09-02 16:53:54 +0530

    add some comment to the code

--- src/queue/usingList/ListQueue.java
+++ src/queue/usingList/ListQueue.java
@@ -106,7 +106,7 @@ public class ListQueue<E> extends AbstractQueue<E> implements QueueADT<E> {
                 rear = new Node<>(item);
                 front = rear;
             } else {
-
+                // Allocate a new node at end, store item in it, and link it to old end of the queue.
             }
         }
     }
