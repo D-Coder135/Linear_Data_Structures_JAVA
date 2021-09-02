2ae6057e42b4784f7242d2d8ec291cf0971f23ba
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-09-02 13:32:24 +0530

    add some comment as a description to the code

--- src/queue/QueueADT.java
+++ src/queue/QueueADT.java
@@ -1,6 +1,14 @@
 package queue;
 
+/**
+ * A queue is a linear data structure which follows a particular order in which the operations are performed.
+ * The order is First-In First-Out (FIFO)
+ *
+ * @param <E> The element type
+ */
+
 public interface QueueADT<E> {
+
     boolean offer(E item);
 
 }
