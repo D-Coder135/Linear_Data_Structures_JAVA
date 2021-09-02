4a02122b7384ddde7ae70d28c3b9cd4b47d91981
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-09-02 16:30:42 +0530

    add a new file ListQueue.java inside the package queue/usingList

---
+++ src/queue/usingList/ListQueue.java
@@ -0,0 +1,11 @@
+/*   Created by IntelliJ IDEA.
+ *   Author: Devansh Shukla (D-Coder135)
+ *   Date: 02/09/21
+ *   Time: 4:30 PM
+ *   File: ListQueue.java
+ */
+
+package queue.usingList;
+
+public class ListQueue {
+}
