0f4721d0b445fae534a420ed420a2192c0b9b449
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-09-02 23:09:27 +0530

    add an if() block along with a condition inside it

--- src/queue/usingList/ListQueue.java
+++ src/queue/usingList/ListQueue.java
@@ -130,6 +130,9 @@ public class ListQueue<E> extends AbstractQueue<E> implements QueueADT<E> {
 
     @Override
     public E peek() {
+        if (size == 0) {
+
+        }
         return null;
     }
 }
