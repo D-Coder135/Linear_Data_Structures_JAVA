9a13c2d10d4e85a8f66289ad7f154cc8a0a91a56
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-09-02 16:55:12 +0530

    update the value for the rear field

--- src/queue/usingList/ListQueue.java
+++ src/queue/usingList/ListQueue.java
@@ -108,6 +108,7 @@ public class ListQueue<E> extends AbstractQueue<E> implements QueueADT<E> {
             } else {
                 // Allocate a new node at end, store item in it, and link it to old end of the queue.
                 rear.next = new Node<>(item);
+                rear = rear.next;
             }
         }
     }
