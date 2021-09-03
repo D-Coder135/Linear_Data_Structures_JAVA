0b540e7bcad2f4caa23314ce290952a01cb9136b
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-09-03 23:41:52 +0530

    implement the required methods inside the class ArrayQueue

--- src/queue/usingArray/ArrayQueue.java
+++ src/queue/usingArray/ArrayQueue.java
@@ -10,6 +10,31 @@ package queue.usingArray;
 import queue.QueueADT;
 
 import java.util.AbstractQueue;
+import java.util.Iterator;
 
 public class ArrayQueue<E> extends AbstractQueue<E> implements QueueADT<E> {
+    @Override
+    public Iterator<E> iterator() {
+        return null;
+    }
+
+    @Override
+    public int size() {
+        return 0;
+    }
+
+    @Override
+    public boolean offer(E e) {
+        return false;
+    }
+
+    @Override
+    public E poll() {
+        return null;
+    }
+
+    @Override
+    public E peek() {
+        return null;
+    }
 }
